b22a12b4244d058322bd0be5d36c05eceac99f8d
lilind17 <38974681+lilind17@users.noreply.github.com>
2018-05-04 10:52:03 +0200

    update index.php
    Skrev in lite text och skapade en kolumn. vill du ha någon mer kolumn eller något annat som ska vara på sidan så kan jag lägga till det?
    mvh Linnea

--- index.php
+++ index.php
@@ -11,8 +11,19 @@ echo $navbar;
         <title>Home</title>
     </head>
     <body>
-        <h1>Test outputs:</h1>
-        <p>Session variables in session:</p>
+        <h1>Welcome</h1>
+          <div class="row">
+            <div class="col-md-6">
+                <div>
+                    <p>Hi and welcome to HealthDashboard. This is a website where you will be able to submit different types of health data 
+                        that will be presented in the dashborad as graphics. You will be able to see, monitor, and get an 
+                        understanding over how your your activity, sleep and temperature has changed over time.< br>
+                        To be able to acces and submit your data you will need to log in or create an account first. 
+                         </p>
+                    </div>
+                </div>
+              
+            </div>
         <?php
         // temporary debug option
         if (isset($_SESSION['username'])) {
